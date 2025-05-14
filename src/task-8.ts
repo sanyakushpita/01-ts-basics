0ffaa2c4e2fd419279e797c139e5aa819db7135a
SaNya <kushpita.o@gmail.com>
2025-05-14 21:48:58 +0300

    fix task-3 and task-8

--- src/task-8.ts
+++ src/task-8.ts
@@ -6,9 +6,13 @@ interface Post {
   body: string;
 }
 
-async function fetchPosts() {
+async function fetchPosts(): Promise<Post[]> {
   const response = await axios.get<Post[]>(
     'https://jsonplaceholder.typicode.com/posts'
   );
   return response.data;
 }
+
+fetchPosts().then((posts) => {
+  console.log(posts[0].title);
+});
